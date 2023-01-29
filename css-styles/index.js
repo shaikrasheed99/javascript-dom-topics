3dbbd5a391bc5160761ebdb126d664a01cfcf63d
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-01-29 21:27:27 +0530

    Add. DOM css styles example

---
+++ css-styles/index.js
@@ -0,0 +1,9 @@
+const body = document.body;
+const para = document.querySelector('.para');
+
+body.style.background = '#131316';
+body.style.color = 'white';
+para.setAttribute('style', 'display: inline-block');
+para.style.color = 'violet';
+para.style.border = '2px solid pink';
+para.style.padding = '5px 30px';
